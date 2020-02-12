422ec30f5c7daf817c6ee26bfe23972cca536088
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-12 01:47:51 -0500

    Expose CRUD operations as REST endpoints

--- server/src/main/java/com/okta/developer/demo/beer/BeerRepository.java
+++ server/src/main/java/com/okta/developer/demo/beer/BeerRepository.java
@@ -1,6 +1,5 @@
-package com.okta.developer.demo.beer;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+@RepositoryRestResource
 interface BeerRepository extends JpaRepository<Beer, Long> {
 }
