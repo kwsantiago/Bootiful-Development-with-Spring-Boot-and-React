572ff73ea6ad584c4e67b2174151662b3711db2d
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-12 01:54:58 -0500

    Update BeerRepository.java

--- server/src/main/java/com/okta/developer/demo/beer/BeerRepository.java
+++ server/src/main/java/com/okta/developer/demo/beer/BeerRepository.java
@@ -1,3 +1,6 @@
+package com.okta.developer.demo.beer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
